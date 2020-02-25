88b287ac2bb05bc52eb3d0bd8af1cc6eae2e2eeb
marcorossignoli20 <marcorossignoli1997@gmail.com>
2020-02-25 11:37:53 +0100

    added some basic error handling and fixed README

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-#CSV to Excel for Spending Tracker app (iPhone)
+# CSV to Excel for Spending Tracker app (iPhone)
 
-Tiny script to convert CSV output file from Spending Tracker app for iPhone to an Excel formatted file
+## Description
+Tiny script to convert CSV output file from [Spending Tracker app for iPhone](https://apps.apple.com/us/app/spending-tracker/id548615579) to a Microsoft Excel (.xlsx) formatted file
+
+## Usage
+[Pip module XlsxWriter](https://pypi.org/project/XlsxWriter/) is **needed** to generate the Excel file
+
+```
+pip install XlsxWriter
+
+python3 csvtoexcel.py
+```
+
+Once the script is launched, you will be prompted to enter the path of the csv file and the desired name for the output Microsoft Excel (.xlsx) file
--- csvtoexcel.py
+++ csvtoexcel.py
@@ -1,48 +1,53 @@
-import xlsxwriter, csv
+import xlsxwriter, csv, os
 
-workbook = xlsxwriter.Workbook('./Spese01.xlsx')
-worksheet = workbook.add_worksheet()
-bold = workbook.add_format({'bold': True})
-money = workbook.add_format({'num_format': '€#,#0'})
-entrate = 0
-uscite = 0
+csv_path = input('Please enter the path of the CSV file: ')
 
-with open('./spese.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    row = 0
-    for line in csv_reader:
-        if row == 0:
-            worksheet.write(row, 0, 'Data', bold)
-            worksheet.write(row, 1, 'Categoria', bold)
-            worksheet.write(row, 2, 'Uscite', bold)
-            worksheet.write(row, 3, 'Entrate', bold)
-            worksheet.write(row, 4, 'Note', bold)
-            row+=1
-        else:
-            if(line[1].lstrip() == 'Gifts'):
-                pass
+if os.path.exists(csv_path): #if csv file path is correct
+    workbook_name = input('Please enter the name of the EXCEL file: ')
+
+    workbook = xlsxwriter.Workbook('./'+workbook_name) #creating workbook
+    worksheet = workbook.add_worksheet()
+    bold = workbook.add_format({'bold': True}) #setting bold format
+    money = workbook.add_format({'num_format': '€#,#0'}) #change the € symbol with the one you prefer
+    incomes = 0
+    expenses = 0
+
+    with open(csv_path) as csv_file: #opening csv file
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        row = 0
+        
+        for line in csv_reader:
+            if row == 0:
+                worksheet.write(row, 0, 'Date', bold)
+                worksheet.write(row, 1, 'Category', bold)
+                worksheet.write(row, 2, 'Expenses', bold)
+                worksheet.write(row, 3, 'Incomes', bold)
+                worksheet.write(row, 4, 'Notes', bold)
+                row+=1
             else:
                 for col in range(4):
                     if col==2:
                         num = float(line[col].lstrip())
                         if num < 0:
                             num = abs(num)
-                            uscite += num
+                            expenses += num
                             worksheet.write(row, col, num, money)
                         else:
                             num = abs(num)
-                            entrate += num
+                            incomes += num
                             worksheet.write(row, col+1, num, money)
                     elif col == 3:
                         worksheet.write(row, col+1, line[col])
                     else:
                         worksheet.write(row, col, line[col])
                 row+=1
-worksheet.write(row, 0, 'Totale:', bold)
-worksheet.write(row, 2, uscite, bold)
-worksheet.write(row, 3, entrate, bold)
-row+=2
-worksheet.write(row, 0, 'Totale mancante: ', bold)
-worksheet.write(row, 1, uscite-entrate, bold)
-workbook.close()
-        
+
+    worksheet.write(row, 0, 'Total amount:', bold)
+    worksheet.write(row, 2, expenses, bold)
+    worksheet.write(row, 3, incomes, bold)
+    row+=2
+    worksheet.write(row, 0, 'Totale missing: ', bold)
+    worksheet.write(row, 1, expenses-incomes, bold)
+    workbook.close()
+else:
+    print('** ERROR: CSV file not found **')
